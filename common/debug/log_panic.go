a3e3a3841b2dd2b826a6c0fc93089b8164fe8541
alex.sharov <AskAlexSharov@gmail.com>
2021-10-21 16:56:54 +0700

    start_pool_at_genesis_on_devnet

--- common/debug/log_panic.go
+++ common/debug/log_panic.go
@@ -16,41 +16,6 @@ func GetSigC(sig *chan os.Signal) {
 	sigc = *sig
 }
 
-/*
-var crashReportDir string
-func prettyTime() string {
-	time := fmt.Sprintf("%v", time.Now())
-	return strings.Replace(time[:19], " ", "-", 1)
-}
-func CheckForCrashes(datadir string) {
-	crashReportDir = filepath.Join(datadir, "crashreports")
-	if _, err := os.Stat(crashReportDir); err != nil && os.IsNotExist(err) {
-		_ = os.MkdirAll(crashReportDir, 0755)
-	} else if err != nil {
-		log.Error("log_panic.go: CheckForCrashes", "error", err)
-		return
-	}
-	f, err := os.Open(crashReportDir)
-	if err != nil {
-		log.Error("log_panic.go: CheckForCrashes", "error", err)
-		return
-	}
-	fileInfo, err := f.ReadDir(-1)
-	if err != nil {
-		log.Error("log_panic.go: CheckForCrashes", "error", err)
-		return
-	}
-	for _, v := range fileInfo {
-		if !v.IsDir() {
-			msg := fmt.Sprintf("Crashes From Previous Boots Detected. Find the stack trace in %v",
-				crashReportDir)
-			log.Warn(msg)
-			_ = f.Close()
-			return
-		}
-	}
-}
-*/
 var panicReplacer = strings.NewReplacer("\n", " ", "\t", "", "\r", "")
 
 // LogPanic - does log panic to logger and to <datadir>/crashreports then stops the process
@@ -72,7 +37,7 @@ func LogPanic() {
 // it returns recovered panic as error in format friendly for our logger
 // common pattern of use - assign to named output param:
 //  func A() (err error) {
-//	    defer func() { err = debug.ReportPanicAndRecover() }() // avoid crash because Erigon's core does many things
+//	    defer func() { err = debug.ReportPanicAndRecover(err) }() // avoid crash because Erigon's core does many things
 //  }
 func ReportPanicAndRecover(err error) error {
 	panicResult := recover()
@@ -87,22 +52,5 @@ func ReportPanicAndRecover(err error) error {
 	default:
 		err = fmt.Errorf("%+v, trace: %s", typed, panicReplacer.Replace(stack))
 	}
-	//log.Error("panic", "err", panicResult, "stack", panicReplacer.Replace(stack))
-	//WriteStackTraceOnPanic(stack)
 	return err
 }
-
-/*
-func WriteStackTraceOnPanic(stack string) {
-	fileName := filepath.Join(crashReportDir, prettyTime()+".txt")
-	f, errFs := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if errFs != nil {
-		log.Error("log_panic.go:WriteStackTraceOnPanic", "error", errFs)
-		f.Close()
-		return
-	}
-	f.WriteString(stack)
-	f.Sync()
-	f.Close()
-}
-*/
